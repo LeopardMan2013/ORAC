6c396fbb2e756a220a310e9d0df3b53f430cc93a
Jack MacDonald <jackmacdonald@Mac.modem>
2025-12-31 21:00:07 +1000

    mango

---
+++ starter_set_1/aio/mango.py
@@ -0,0 +1,7 @@
+# make them input 4 numbers - (i pos, c pos, how far is i from m?, how far is c from m?)
+# find a mathmatical way to solve "m pos"
+# print "m pos"
+# they're on a line
+# "pos is measured from the left"
+
+a, b, c, d = map(int, input().split())
