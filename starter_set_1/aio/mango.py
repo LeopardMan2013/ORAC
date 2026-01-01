302a50f1fc5e2f223aedc3c3e6f1bac9a2ce7086
Jack MacDonald <jackmacdonald@Mac.modem>
2026-01-01 10:20:22 +1000

    mang problem

--- starter_set_1/aio/mango.py
+++ starter_set_1/aio/mango.py
@@ -1,7 +1,19 @@
-# make them input 4 numbers - (i pos, c pos, how far is i from m?, how far is c from m?)
-# find a mathmatical way to solve "m pos"
-# print "m pos"
-# they're on a line
-# "pos is measured from the left"
+Ix, Cx, Id, Cd = map(int, input().split())
 
-a, b, c, d = map(int, input().split())
+# # print(f"Ix {Ix} Cx {Cx} Id {Id} Cd {Cd}")
+
+# # Ix is position of I; Cx is position of C.
+# # Id is how far the mango is from I; Cd from C
+# # Id and Cd a positive ints - so refer to 2 poistions - 1 left and 1 right - rel to Ix and Cx
+# # Therefore, we compute 4 possible positions of M, 2 rel to Ix and 2 rel to Cx
+# # The output in the position rel to Ix that is also an output rel to Cx.
+
+# # we will use I1 and I2 to be rhe 2 possible positions relative to I
+
+I_possibilities = {Ix - Id, Ix + Id}
+C_possibilities = {Cx - Cd, Cx + Cd}
+
+# print(I_possibilities)
+# print(C_possibilities)
+# print(I_possibilities.intersection(C_possibilities))
+print(list(I_possibilities.intersection(C_possibilities))[0])
